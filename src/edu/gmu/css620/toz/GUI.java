d950eda8921ead6d6132320c10a6796eddb8db1c
oztalha <toz@gmu.edu>
2014-12-03 15:28:42 -0500

    Scale-Independent added

--- src/edu/gmu/css620/toz/GUI.java
+++ src/edu/gmu/css620/toz/GUI.java
@@ -1,6 +1,12 @@
 package edu.gmu.css620.toz;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
+import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -10,6 +16,7 @@ import sim.display.Controller;
 import sim.display.Display2D;
 import sim.display.GUIState;
 import sim.engine.SimState;
+import sim.portrayal.continuous.ContinuousPortrayal2D;
 import sim.portrayal.grid.SparseGridPortrayal2D;
 import sim.portrayal.simple.ImagePortrayal2D;
 import sim.util.Bag;
@@ -19,6 +26,7 @@ public class GUI extends GUIState {
 	public Display2D display;
 	public JFrame displayFrame;
 	SparseGridPortrayal2D agentsPortrayal = new SparseGridPortrayal2D();
+	//ContinuousPortrayal2D agentsPortrayal = new ContinuousPortrayal2D();
 	
 	public GUI() {super(new PG(System.currentTimeMillis()));}
 	public GUI(SimState state) {super(state);}
@@ -42,15 +50,28 @@ public class GUI extends GUIState {
 	
 	public void setupPortrayals() {
 		PG pg = (PG) state;
+		agentsPortrayal.setField(pg.world);
+		//agentsPortrayal.setField(pg.eurasia);
+
 		System.out.println("Working Directory = " + System.getProperty("user.dir"));
 		 
 		// tell the portrayals what to portray and how to portray them
 		// http://cs.gmu.edu/~eclab/projects/mason/docs/howto.html#display2d
-		agentsPortrayal.setField(pg.world);
-		ImageIcon eurasia = new ImageIcon("eurasia.gif");
-		int dimension = (int)Math.min(pg.world.getWidth(),pg.world.getHeight());
-		agentsPortrayal.setPortrayalForAll(new ImagePortrayal2D(eurasia, dimension));
-		display.attach(agentsPortrayal, "Eurasia");
+
+		// Auto-Scaling and Auto-Scrolling 
+//		ImageIcon eurasia = new ImageIcon("eurasia.gif");
+//		int dimension = (int)Math.min(pg.eurasia.getWidth(),pg.eurasia.getHeight());
+//		agentsPortrayal.setPortrayalForAll(new ImagePortrayal2D(eurasia.getImage(), dimension));
+//		display.attach(agentsPortrayal, "Eurasia");
+//		display.setBackdrop(null);
+		
+		// Scale-Independent
+		Image i = new ImageIcon("eurasia.gif").getImage();
+		BufferedImage b = display.getGraphicsConfiguration().createCompatibleImage(i.getWidth(null), i.getHeight(null));
+		Graphics g = b.getGraphics();
+		g.drawImage(i,0,0,i.getWidth(null),i.getHeight(null),null);
+		g.dispose();
+		display.setBackdrop(new TexturePaint(b, new Rectangle(0,0,i.getWidth(null),i.getHeight(null))));
 		
 		Bag agents = pg.world.getAllObjects();
 		for (Object a : agents) {
@@ -63,13 +84,12 @@ public class GUI extends GUIState {
 		
 		// reschedule the displayer
         display.reset(); 
-        display.setBackdrop(null);
         display.repaint();
 	}
 	
 	public void init(Controller c) {
 		super.init(c);
-		display = new Display2D(600,600,this);
+		display = new Display2D(516,486,this);
 		display.setClipping(false);
 		displayFrame = display.createFrame();
 		displayFrame.setTitle("PG-ABM");
--- src/edu/gmu/css620/toz/PG.java
+++ src/edu/gmu/css620/toz/PG.java
@@ -1,25 +1,31 @@
 package edu.gmu.css620.toz;
 
 import sim.engine.SimState;
+import sim.field.continuous.Continuous2D;
 import sim.field.grid.SparseGrid2D;
+import sim.util.Double2D;
 
 public class PG extends SimState {
 
 	private static final long serialVersionUID = 1L;
 	public SparseGrid2D world;
-	public int gridWidth = 100; // width of the space
-	public int gridHeight = 100; // Height of the space
+	//public Continuous2D eurasia;
+	
+	public int gridWidth = 516/4; // width of the space
+	public int gridHeight = 486/4; // Height of the space
 	public int n = 100; //number of agents
 	
 	public PG(long seed) {super(seed);}
 	public void start() {
 		super.start();
+		//eurasia = new Continuous2D(gridWidth*gridHeight/100, gridWidth, gridHeight);
 		world = new SparseGrid2D(gridWidth, gridHeight);
 		for(int i=0;i<n;i++){
 			Agent a = new Agent();
 			int x = random.nextInt(gridWidth);
 			int y = random.nextInt(gridHeight); 
 			world.setObjectLocation(a, x, y);
+			//eurasia.setObjectLocation(a, new Double2D(x, y));
 			schedule.scheduleRepeating(a);
 		}
 	}
